01b7786f95956196085818b8f371f0aab0749267
Gustav Holfve <gustav.holfve@redmind.se>
2019-09-30 18:58:40 +0200

    update typings

--- dist/components/Image/image.component.d.ts
+++ dist/components/Image/image.component.d.ts
@@ -1,12 +1,12 @@
-import { Component } from "react";
+import React from "react";
 import { FastImageProperties, FastImageSource } from "react-native-fast-image";
 export interface CdnImageProps extends FastImageProperties {
     imageFormat: "jpg" | "png" | "webp";
     normalize: boolean;
 }
 export declare type CdnImageState = {};
-export declare class CdnImage extends Component<CdnImageProps, CdnImageState> {
-    static defaulProps: Partial<CdnImageProps>;
+export declare class CdnImage extends React.Component<CdnImageProps, CdnImageState> {
+    static defaultProps: Partial<CdnImageProps>;
     generateSourceUrl(source: FastImageSource | number): FastImageSource | number;
     render(): JSX.Element;
 }
