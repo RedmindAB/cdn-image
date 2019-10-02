c993929d329f5f915696f8f626c4863f52bf68c0
Gustav Holfve <gustav.holfve@redmind.se>
2019-10-02 11:16:41 +0200

    Star import React instead of default

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Image_1 = require("./components/Image");
 exports.CdnImage = Image_1.CdnImage;
+exports.default = Image_1.CdnImage;
